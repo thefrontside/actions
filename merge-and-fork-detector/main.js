807789eb53d09d8dc2f5332c0788850f6f826463
minkimcello <minkimcello@gmail.com>
2019-09-30 17:33:56 -0400

    console log when input is not provided

--- merge-and-fork-detector/main.js
+++ merge-and-fork-detector/main.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const NPM_AUTH_TOKEN = core.getInput('NPM_AUTH_TOKEN');
 const octokit = new github.GitHub(GITHUB_TOKEN);
 
 // check conmmit to see merged by looking for #1-9
@@ -9,25 +10,24 @@ const octokit = new github.GitHub(GITHUB_TOKEN);
 
 async function run() {
   try {
-    // let commit_regexed = /#(\d+)/.exec(github.context.payload.head_commit.message)[1];
+    const regex = /#(\d+)/;
+    const commit_message = github.context.payload.head_commit.message;
 
-    const pull_request = await octokit.pulls.get({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      // state: 'closed', //because we only care about merged
-      // head: github.context.ref //only pull request merged to the branch we're on
-      pull_number: /#(\d+)/.exec(github.context.payload.head_commit.message)[1]
-    });
-
-    console.log(pull_request);
-
-    // const match_commit = pullrequests.data.filter((pr) => { return pr.merged_at != null })[0];
-
-    // console.log("Retrieving labels from merge #" + lastmerged.number + ".");
-
-    // const labels = lastmerged.labels.map(labels => { return labels.name });
-
-    // core.setOutput('PR_LABELS', labels);
+    if (github.context.payload.repository.fork) {
+      console.log("This is a forked repository")
+      console.log(NPM_AUTH_TOKEN);
+    } else {
+      if (regex.test(commit_message)) {
+        const pull_request = await octokit.pulls.get({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          pull_number: regex.exec(commit_message)[1]
+        });
+        console.log(pull_request);
+      } else {
+        console.log("We suspect this workflow wasn't triggered by a commit made from a merge because we could not locate a PR number in the commit message.")
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   };
