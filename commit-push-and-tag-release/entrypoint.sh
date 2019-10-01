a4e5530da668632599c2e2a35b483a87d249e971
minkimcello <minkimcello@gmail.com>
2019-10-01 11:58:07 -0400

    commented out rest for just echo branch

--- commit-push-and-tag-release/entrypoint.sh
+++ commit-push-and-tag-release/entrypoint.sh
@@ -3,16 +3,18 @@ set -euo pipefail
 
 git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
 
-# git fetch origin +refs/heads/*:refs/heads/*
-# branch=$(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
-# git checkout $branch
-# echo branch: $branch
+git fetch origin +refs/heads/*:refs/heads/*
+branch=$(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
+git checkout $branch
+echo branch: $branch
 
-git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-git config user.name "$GITHUB_ACTOR"
+# ___ uncomment below
 
-git add $INPUT_ADD
+# git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+# git config user.name "$GITHUB_ACTOR"
 
-current="`node -e \"console.log(require('./package.json').version)\"`"
-git commit -m "Release version $current"
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" #$branch
+# git add $INPUT_ADD
+
+# current="`node -e \"console.log(require('./package.json').version)\"`"
+# git commit -m "Release version $current"
+# git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" #$branch
