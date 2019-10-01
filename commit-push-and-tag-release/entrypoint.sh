4a3cfde175c661646337dd2f701604cf1a8531d0
minkimcello <minkimcello@gmail.com>
2019-10-01 12:45:19 -0400

    repo should have test file

--- commit-push-and-tag-release/entrypoint.sh
+++ commit-push-and-tag-release/entrypoint.sh
@@ -4,24 +4,24 @@ set -euo pipefail
 git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
 git fetch origin +refs/heads/*:refs/heads/*
 
-echo referee: "${GITHUB_REF#*refs\/heads\/}"
+branch="${GITHUB_REF#*refs\/heads\/}"
+git checkout $branch
 
+### outdated
 # branch=$(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
-# echo echoing what branch just did: $(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
-
-# echo before checkout: $(git branch | grep \* | cut -d ' ' -f2)
 # git checkout $GITHUB_HEAD_REF
-# echo current branch: $(git branch | grep \* | cut -d ' ' -f2)
+
+
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git config user.name "$GITHUB_ACTOR"
+
+echo hello > test.md
+# git add $INPUT_ADD
+git add test.md
+
+current="`node -e \"console.log(require('./package.json').version)\"`"
+git commit -m "Release version $current"
+
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" $branch
 
 exit 1
-
-# ___ uncomment below
-
-# git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-# git config user.name "$GITHUB_ACTOR"
-
-# git add $INPUT_ADD
-
-# current="`node -e \"console.log(require('./package.json').version)\"`"
-# git commit -m "Release version $current"
-# git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" #$branch
