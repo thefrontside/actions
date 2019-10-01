f319523cc8ced9cdc24657372e62281167d5a129
minkimcello <minkimcello@gmail.com>
2019-10-01 12:24:03 -0400

    just ref

--- commit-push-and-tag-release/entrypoint.sh
+++ commit-push-and-tag-release/entrypoint.sh
@@ -2,16 +2,18 @@
 set -euo pipefail
 
 git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
-
 git fetch origin +refs/heads/*:refs/heads/*
-echo report after fetch
 
-branch=$(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
-echo echoing what branch just did: $(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
+echo referee: $GITHUB_REF
 
-echo before checkout: $(git branch | grep \* | cut -d ' ' -f2)
-git checkout $branch
-echo current branch: $(git branch | grep \* | cut -d ' ' -f2)
+# branch=$(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
+# echo echoing what branch just did: $(printf "%s\n" "${GITHUB_BASE_REF#*refs\/heads\/}")
+
+# echo before checkout: $(git branch | grep \* | cut -d ' ' -f2)
+# git checkout $GITHUB_HEAD_REF
+# echo current branch: $(git branch | grep \* | cut -d ' ' -f2)
+
+exit 1
 
 # ___ uncomment below
 
