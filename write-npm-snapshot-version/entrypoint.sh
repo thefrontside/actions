1db9a8099b6343fa1dfd08e9869629618e7e9449
minkimcello <minkimcello@gmail.com>
2019-10-01 13:16:45 -0400

    tag is useless in version

--- write-npm-snapshot-version/entrypoint.sh
+++ write-npm-snapshot-version/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version --tag dev
+npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
