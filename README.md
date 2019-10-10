2a9bb80a434dbbad47b2aaf7f9b336b33a6b16eb
Min Kim <minkimcello@gmail.com>
2019-10-10 12:32:46 -0400

    Introduce npm-publish-branch-preview and synchronize-npm-tags (#13)
    * Create npm-publish-branch-preview and synchronize-npm-tags
    
    * Add IFS for synchronize-with-npm
    
    * Update message color for synchronize-npm-tags

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ The following are our current actions. Go to its directories for a more detailed
 
 | Actions | Description |
 | ------- | ----------- |
-| [merge-and-fork-detector](/merge-and-fork-detector) | Halts workflow for certain scenarios for open source projects. |
+| [npm-publish-branch-preview](/npm-publish-branch-preview) | Publish package with tag using the name of the head branch.|
 | [post-npm-usage-instructions-comment](/post-npm-usage-instructions-comment) | Generate comment on pull request with instructions on how to use the newly published package. |
+| [synchronize-npm-tags](/synchronize-npm-tags) | Remove unwanted tags from NPM. |
 | [synchronize-with-npm](/synchronize-with-npm) | Push tag to Github and Publish to NPM. |
 | [write-npm-snapshot-version](/write-npm-snapshot-version) | Updates package version in `package.json` with the SHA of the current commit. |
---
+++ synchronize-npm-tags/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+IFS=$'\n\t' #required for checking tag against user argument
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+package="`node -e \"console.log(require('./package.json').name)\"`"
+branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')"
+npmtags=$(npm dist-tag ls | sed 's/\:.*//')
+
+for tag in $npmtags; do
+  if [[ "$tag" = "latest" ]] || [[ $(echo "$INPUT_KEEP" | grep -e "$tag") ]]
+    then
+      echo -e "${RED}$tag${GREEN}: Keeping protected tag.${NC}"
+  elif [[ $(echo $branches | grep -e "$tag") ]]
+    then
+      echo -e "${RED}$tag${GREEN}: Keeping tag because we found a matching branch.${NC}"
+  else
+    echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+    npm dist-tag rm $package $tag
+    echo -e "${RED}$tag${YELLOW}: Removed tag from NPM because it did not match any existing branches.${NC}"
+  fi
+done
