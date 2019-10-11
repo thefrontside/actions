f833c727b90dd4076f4e1f458a2a00c963bf8103
minkimcello <minkimcello@gmail.com>
2019-10-11 15:15:49 -0400

    Implement tag-encoding to avoid npm errors

--- synchronize-npm-tags/entrypoint.sh
+++ synchronize-npm-tags/entrypoint.sh
@@ -8,20 +8,22 @@ YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m'
 
-package="`node -e \"console.log(require('./package.json').name)\"`"
-branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')"
-npmtags=$(npm dist-tag ls | sed 's/\:.*//')
+package="`node -e \"console.log(require('./package.json').name)\"`";
+input_keep_encoded="$(echo $INPUT_KEEP | sed -E 's/\_+/&\_/g;s/\//\_/g')"
+branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')";
+branches_encoded="$(echo $branches | sed -E 's/\_+/&\_/g;s/\//\_/g')";
+npmtags=$(npm dist-tag ls | sed 's/\:.*//');
 
 for tag in $npmtags; do
-  if [[ "$tag" = "latest" ]] || [[ $(echo "$INPUT_KEEP" | grep -e "$tag") ]]
+  if [[ "$tag" = "latest" ]] || [[ $(echo "$input_keep_encoded" | grep -e "$tag") ]]
     then
-      echo -e "${RED}$tag${GREEN}: Keeping protected tag.${NC}"
-  elif [[ $(echo $branches | grep -e "$tag") ]]
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
+  elif [[ $(echo $branches_encoded | grep -e "$tag") ]]
     then
-      echo -e "${RED}$tag${GREEN}: Keeping tag because we found a matching branch.${NC}"
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
   else
     echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
     npm dist-tag rm $package $tag
-    echo -e "${RED}$tag${YELLOW}: Removed tag from NPM because it did not match any existing branches.${NC}"
+    echo -e "${RED}Removed tag, ${YELLOW}$tag${RED} from NPM because it did not match any existing branches.${NC}"
   fi
 done
