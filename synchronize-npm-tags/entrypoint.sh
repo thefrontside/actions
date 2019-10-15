d12762b2efa223f60ad363b84499621366b19b8d
minkimcello <minkimcello@gmail.com>
2019-10-15 17:20:50 -0400

    Make npm-tags check against each branch/arg in for loop

--- synchronize-npm-tags/entrypoint.sh
+++ synchronize-npm-tags/entrypoint.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 set -e
-IFS=$'\n\t' #required for checking tag against user argument
+IFS=$'\n\t'
 
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
 NC='\033[0m'
 
 package="`node -e \"console.log(require('./package.json').name)\"`";
-input_keep_encoded="$(echo $INPUT_KEEP | sed -E 's/\_/\_\_/g;s/\//\_/g')"
+input_encoded="$(echo $INPUT_PRESERVE | sed -E 's:_:__:g;s:\/:_:g')"
+declare -a input_arrayed=($(echo $input_encoded | tr " " "\n"))
 branches="$(git ls-remote --heads origin  | sed 's?.*refs/heads/??')";
-branches_encoded="$(echo $branches | sed -E 's/\_/\_\_/g;s/\//\_/g')";
-npmtags=$(npm dist-tag ls | sed 's/\:.*//');
+branches_encoded="$(echo $branches | sed -E 's:_:__:g;s:\/:_:g')";
+declare -a branches_arrayed=($(echo $branches_encoded | tr " " "\n"));
+npmtags=$(npm dist-tag ls | sed 's/:.*//');
 
 for tag in $npmtags; do
-  if [[ "$tag" = "latest" ]] || [[ $(echo "$input_keep_encoded" | grep -e "$tag") ]]
+  if [[ "$tag" = "latest" ]]
     then
       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
-  elif [[ $(echo $branches_encoded | grep -e "$tag") ]]
+  elif [[ $(echo $(for branch in ${branches_arrayed[@]}; do if [[ "$branch" = "$tag" ]]; then echo "$tag"; fi; done;)) ]]
     then
       echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because we found a matching branch.${NC}"
+  elif [[ $(echo $(for arg in ${input_arrayed[@]}; do if [[ "$arg" = "$tag" ]]; then echo "$tag"; fi; done;)) ]]
+    then
+      echo -e "${GREEN}Keeping tag, ${YELLOW}$tag${GREEN}, because it is protected.${NC}"
   else
     echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
     npm dist-tag rm $package $tag
