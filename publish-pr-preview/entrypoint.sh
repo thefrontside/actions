ace529984d682b02496a5d21a6885d5b4c834e14
minkimcello <minkimcello@gmail.com>
2019-10-15 18:12:12 -0400

    Update message content

--- publish-pr-preview/entrypoint.sh
+++ publish-pr-preview/entrypoint.sh
@@ -30,14 +30,19 @@ const { markdown } = require('danger');
 const pjson = require('./package.json');
 
 const current = `https://www.npmjs.com/package/${pjson.name}/v/${pjson.version}`
+const branch = process.env.GITHUB_HEAD_REF;
+const masked = branch.replace(/\//g, '_');
 
-
-const first_line = `This PR is available to use:`
 const install_version = `npm install ${pjson.name}@${pjson.version}`;
-const install_tag = `npm install ${pjson.name}@${process.env.GITHUB_HEAD_REF}`;
-const last_line = `You can view the NPM package [here](${current}).`
 
-markdown(`${first_line}\n\`\`\`bash\n${install_version}\n\`\`\`\nor\n\`\`\`bash\n${install_tag}\n\`\`\`\n${last_line}`)
+const first_line = `A preview package of this pull request has been released to NPM with the tag \`${masked}\`.`;
+const second_line = `You can try it out by running the following command:`;
+const install_tag = `$ npm install ${pjson.name}@${masked}`;
+const fourth_line = `or by updating your package.json to:`
+const update_json = `\{\n  \"${pjson.name}\": \"${masked}\"\n\}`
+const last_line = `Once the branch associated with this tag is deleted (usually once the PR is merged or closed), it will no longer be available. However, it currently references [${pjson.name}@${pjson.version}](${current}) which will be available to install forever.`;
+
+markdown(`${first_line}\n${second_line}\n\`\`\`bash\n${install_tag}\n\`\`\`\n${fourth_line}\n\`\`\`bash\n${update_json}\n\`\`\`\n${last_line}`)
 EOT
   yarn global add danger --dev
   export PATH="$(yarn global bin):$PATH"
