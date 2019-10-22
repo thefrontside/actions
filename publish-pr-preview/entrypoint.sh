4c3ef0465b16e3984b4bfd1dcdb8fd84e7be59c0
minkimcello <minkimcello@gmail.com>
2019-10-22 15:28:45 -0400

    Remove exit codes and use pr comment instead

--- publish-pr-preview/entrypoint.sh
+++ publish-pr-preview/entrypoint.sh
@@ -10,16 +10,43 @@ if [ "${#NPM_AUTH_TOKEN}" -eq "0" ]
   then
     echo -e "${RED}ERROR: NPM_AUTH_TOKEN not detected. Please add your NPM Token to your repository's secrets.${NC}"
     echo -e "${YELLOW}Publishing preview will not work if the pull request was created from a forked repository unless you create the pull request against your own repository.${NC}"
-    exit 1
+    
+cat << "EOT" > dangerfile.js
+const { markdown } = require('danger');
+
+const first_line = `ERROR: NPM_AUTH_TOKEN not detected. Please add your NPM Token to your repository's secrets.`;
+const second_line = `Publishing preview will not work if the pull request was created from a forked repository unless you create the pull request against your own repository.`;
+
+markdown(`${first_line}\n\n${second_line}`)
+EOT
+
+    yarn global add danger --dev
+    export PATH="$(yarn global bin):$PATH"
+    danger ci
+
 elif [[ "$GITHUB_HEAD_REF" = "" ]]
   then
     echo -e "${RED}ERROR: We suspect this workflow was not triggered from a pull request.${NC}"
     exit 1
+    
 elif [[ "$GITHUB_HEAD_REF" = "latest" ]]
   then
     echo -e "${RED}ERROR: Unable to publish preview because your branch conflicts with NPM's protected 'latest' tag.${NC}"
     echo -e "${YELLOW}Please change the name of your branch and resubmit the pull request.${NC}"
-    exit 1
+
+cat << "EOT" > dangerfile.js
+const { markdown } = require('danger');
+
+const first_line = `ERROR: Unable to publish preview because your branch conflicts with NPM's protected 'latest' tag.`;
+const second_line = `Please change the name of your branch and resubmit the pull request.`;
+
+markdown(`${first_line}\n\n${second_line}`)
+EOT
+
+    yarn global add danger --dev
+    export PATH="$(yarn global bin):$PATH"
+    danger ci
+
 else
   npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
   echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
@@ -32,6 +59,7 @@ else
     else
       $INPUT_NPM_PUBLISH --access=public --tag $tag
   fi
+
 cat << "EOT" > dangerfile.js
 const { markdown } = require('danger');
 const pjson = require('./package.json');
@@ -52,7 +80,8 @@ const last_line = `Once the branch associated with this tag is deleted (usually 
 markdown(`${first_line}\n${second_line}\n\`\`\`bash\n${install_tag}\n\`\`\`\n${fourth_line}\n\`\`\`bash\n${update_json}\n\`\`\`\n${last_line}`)
 EOT
 
-yarn global add danger --dev
-export PATH="$(yarn global bin):$PATH"
-danger ci
+  yarn global add danger --dev
+  export PATH="$(yarn global bin):$PATH"
+  danger ci
+
 fi
