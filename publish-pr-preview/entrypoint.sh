d11b5ee451c839e180c655435924890be70c3c29
minkimcello <minkimcello@gmail.com>
2019-10-22 15:38:25 -0400

    Rearrange if to make code tidier

--- publish-pr-preview/entrypoint.sh
+++ publish-pr-preview/entrypoint.sh
@@ -6,10 +6,16 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-if [ "${#NPM_AUTH_TOKEN}" -eq "0" ]
+if [[ "$GITHUB_HEAD_REF" = "" ]]
   then
-    echo -e "${RED}ERROR: NPM_AUTH_TOKEN not detected. Please add your NPM Token to your repository's secrets.${NC}"
-    echo -e "${YELLOW}Publishing preview will not work if the pull request was created from a forked repository unless you create the pull request against your own repository.${NC}"
+    echo -e "${RED}ERROR: We suspect this workflow was not triggered from a pull request.${NC}"
+    exit 1
+
+else
+  if [ "${#NPM_AUTH_TOKEN}" -eq "0" ]
+    then
+      echo -e "${RED}ERROR: NPM_AUTH_TOKEN not detected. Please add your NPM Token to your repository's secrets.${NC}"
+      echo -e "${YELLOW}Publishing preview will not work if the pull request was created from a forked repository unless you create the pull request against your own repository.${NC}"
     
 cat << "EOT" > dangerfile.js
 const { markdown } = require('danger');
@@ -20,19 +26,10 @@ const second_line = `Publishing preview will not work if the pull request was cr
 markdown(`${first_line}\n\n${second_line}`)
 EOT
 
-    yarn global add danger --dev
-    export PATH="$(yarn global bin):$PATH"
-    danger ci
-
-elif [[ "$GITHUB_HEAD_REF" = "" ]]
-  then
-    echo -e "${RED}ERROR: We suspect this workflow was not triggered from a pull request.${NC}"
-    exit 1
-    
-elif [[ "$GITHUB_HEAD_REF" = "latest" ]]
-  then
-    echo -e "${RED}ERROR: Unable to publish preview because your branch conflicts with NPM's protected 'latest' tag.${NC}"
-    echo -e "${YELLOW}Please change the name of your branch and resubmit the pull request.${NC}"
+  elif [[ "$GITHUB_HEAD_REF" = "latest" ]]
+    then
+      echo -e "${RED}ERROR: Unable to publish preview because your branch conflicts with NPM's protected 'latest' tag.${NC}"
+      echo -e "${YELLOW}Please change the name of your branch and resubmit the pull request.${NC}"
 
 cat << "EOT" > dangerfile.js
 const { markdown } = require('danger');
@@ -43,22 +40,18 @@ const second_line = `Please change the name of your branch and resubmit the pull
 markdown(`${first_line}\n\n${second_line}`)
 EOT
 
-    yarn global add danger --dev
-    export PATH="$(yarn global bin):$PATH"
-    danger ci
-
-else
-  npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
-  echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-  npm config set unsafe-perm true
-  npm install
-  tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
-  if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]
-    then
-      npm publish --access=public --tag $tag
-    else
-      $INPUT_NPM_PUBLISH --access=public --tag $tag
-  fi
+  else
+    npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
+    echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+    npm config set unsafe-perm true
+    npm install
+    tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
+    if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]
+      then
+        npm publish --access=public --tag $tag
+      else
+        $INPUT_NPM_PUBLISH --access=public --tag $tag
+    fi
 
 cat << "EOT" > dangerfile.js
 const { markdown } = require('danger');
@@ -79,6 +72,8 @@ const last_line = `Once the branch associated with this tag is deleted (usually 
 
 markdown(`${first_line}\n${second_line}\n\`\`\`bash\n${install_tag}\n\`\`\`\n${fourth_line}\n\`\`\`bash\n${update_json}\n\`\`\`\n${last_line}`)
 EOT
+
+  fi
 
   yarn global add danger --dev
   export PATH="$(yarn global bin):$PATH"
