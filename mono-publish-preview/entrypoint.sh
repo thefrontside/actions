fd4990f139d559cfc1ed974414dcbf8026dc7601
minkimcello@gmail.com <minkimcello@gmail.com>
2019-11-06 10:54:25 -0600

    with authentication here

--- mono-publish-preview/entrypoint.sh
+++ mono-publish-preview/entrypoint.sh
@@ -26,6 +26,7 @@ function rundanger(){
 
 function publishgateway(){
   cd gateways/pro-portal
+  echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
   npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
   npm publish --tag $tag
   cd $GITHUB_WORKSPACE
