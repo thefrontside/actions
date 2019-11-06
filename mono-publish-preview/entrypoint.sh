2f9bd952ba9213c391caa3eb57a5b1934ad71fc9
minkimcello@gmail.com <minkimcello@gmail.com>
2019-11-06 14:16:22 -0600

    debugging ls

--- mono-publish-preview/entrypoint.sh
+++ mono-publish-preview/entrypoint.sh
@@ -69,7 +69,10 @@ EOT
     else
       for dir in ${confirmedpkgs[@]}; do
         echo publish loop of $dir
-        cd $GITHUB_WORKSPACE/$dir
+        cd $GITHUB_WORKSPACE/"$dir"
+        echo before ls
+        ls
+        echo afterr ls
 
         pkgname="`node -e \"console.log(require('./package.json').name)\"`"
         pkgver="`node -e \"console.log(require('./package.json').version)\"`"
