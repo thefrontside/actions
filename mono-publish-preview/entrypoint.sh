6958d9f40a8de9a93182c2257f56af1d7da2d20e
minkimcello@gmail.com <minkimcello@gmail.com>
2019-11-06 07:07:01 -0600

    just root

--- mono-publish-preview/entrypoint.sh
+++ mono-publish-preview/entrypoint.sh
@@ -8,7 +8,7 @@ NC='\033[0m'
 
 function doublechecky(){
   echo test publishing
-  cd services/Consumer
+#  cd services/Consumer
   echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> .npmrc
   npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
   npm publish
