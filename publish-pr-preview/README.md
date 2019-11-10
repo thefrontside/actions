3ede3550b3602b50f3e54970ce69986168f1e41c
minkimcello@gmail.com <minkimcello@gmail.com>
2019-11-10 10:55:40 -0500

    Integrate monorepo support to publish-pr-preview

--- publish-pr-preview/README.md
+++ publish-pr-preview/README.md
@@ -1,14 +1,20 @@
 # Publish PR Preview
-This action will:
-- Check for accessibility to NPM token in secrets.
-- Make sure this action is being run from a pull request.
-- Make sure the name of the head branch of the pull request isn't `latest` to avoid conflicts with NPM tagging.
-- Update package version with snapshot, publish to NPM with branch name as tag.
-- Comment on pull request with instructions on how to access the package.
+This action will publish packages with the changes proposed in the pull request so that developers do not need for it to be reviewed, approved, and merged. Once the preview packages are published the action generates a comment with instructions on how to access the packages.
+
+## Monorepo Support
+This action now supports monorepos and will do all of the heavy lifting without burdening the developer with any complicated setup process. The action automatically detects which files have been modified and will only publish packages that have changes proposed as to not use up unnecessary resources.
 
 ## Requirements
-- Pass in `GITHUB_TOKEN` and `NPM_AUTH_TOKEN`.
-- Optional: `NPM_PUBLISH` argument is available if you want to run a different command. `npm publish` will run as default.
+- Meant to only run on a pull request
+- Pass in `GITHUB_TOKEN`
+- Pass in `NPM_AUTH_TOKEN`
+- Optional: Set `GPR` to true if you want to use Github Package Registry otherwise it will publish to `npmjs`
+  - Setting `GPR` to true would mean you do not need to pass in `NPM_AUTH_TOKEN`
+- Optional: `NPM_PUBLISH` argument is available if you want to run a different command
+  - `npm publish` will run as default
+- Optional: Specify `IGNORE` argument for directory of packages you don't want published for monorepos
+  - Directories specified in `.gitignore` won't be processed to begin with so for example if you don't want to include `./node_modules` and it's already included in your `.gitignore` file then there's no need to add it to the `IGNORE` arg in the workflow
+  - Root is intentionally not recognized as an argument because in the case of a monorepo it will automatically skip root during its publishing process. And for the case where it is a single repository, we should not be skipping root.
 
 ## Usage
 ```yaml
@@ -22,6 +28,7 @@ jobs:
       uses: thefrontside/actions/publish-pr-preview@master
       with:
         NPM_PUBLISH: npm run my-script
+        IGNORE: folder/example_package folder/example_package2
       env:
         GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
         NPM_AUTH_TOKEN: ${{ secrets.NPM_AUTH_TOKEN }}
--- publish-pr-preview/entrypoint.sh
+++ publish-pr-preview/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
 RED='\033[1;31m'
@@ -6,54 +6,66 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+function run_danger(){
+  yarn global add danger --dev
+  export PATH="$(yarn global bin):$PATH"
+  danger ci
+}
 
-PR="$(jq '."pull_request"' ../workflow/event.json)"
-if [[ "$PR" = "null" ]]
-  then
-    echo -e "${RED}ERROR: We suspect this workflow was not triggered from a pull request.${NC}"
-    exit 1
+function publish(){
+  function install_with_CLI(){
+    if [ -f "yarn.lock" ]; then
+      yarn
+    else
+      npm config set unsafe-perm true
+      npm install
+    fi
+  }
 
-else
-  if [[ "$GITHUB_HEAD_REF" = "latest" ]]
-    then
-      echo -e "${RED}ERROR: Unable to publish preview because your branch conflicts with NPM's protected 'latest' tag.${NC}"
-      echo -e "${YELLOW}Please change the name of your branch and resubmit the pull request.${NC}"
+  function authenticate_publish(){
+    if [ $INPUT_GPR = true ]; then
+      echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" > ~/.npmrc
+    else
+      echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+    fi
+  }
 
-cat << "EOT" > dangerfile.js
-const { markdown } = require('danger');
-
-const first_line = `:warning: WARNING :warning:`;
-const second_line = `I can't publish a preview of this branch this because it would conflict with the \`latest\` tag which is [reserved by NPM](https://docs.npmjs.com/cli/dist-tag#purpose).`;
-
-markdown(`${first_line}\n\n${second_line}`)
-EOT
-
-  elif [ "${#NPM_AUTH_TOKEN}" -eq "0" ]
-    then
-      echo -e "${RED}ERROR: NPM_AUTH_TOKEN not detected. Please add your NPM Token to your repository's secrets.${NC}"
-    
-cat << "EOT" > dangerfile.js
-const { markdown } = require('danger');
-
-const first_line = `:mega: Heads up!`;
-const second_line = `I didn't detect an NPM_AUTH_TOKEN which is necessary to publish a preview version of this package, and so I wasn't able to. However, this is perfectly normal for pull requests that are submitted from a forked repository, so no need to worry if that's what's going on here.`;
-
-markdown(`${first_line}\n\n${second_line}`)
-EOT
-
-  else
+  function npm_version_SHA(){
     npm version "`node -e \"console.log(require('./package.json').version)\"`-`git log --pretty=format:'%h' -n 1`" --no-git-tag-version
-    echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
-    npm config set unsafe-perm true
-    npm install
-    tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
-    if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]
-      then
+  }
+
+  tag="$(echo $GITHUB_HEAD_REF | sed -E 's:_:__:g;s:\/:_:g')"
+  echo '{"tag":"","packages":[]}' > published.json
+  echo $(jq --arg TEST "$tag" '.tag = $TEST' published.json) > published.json
+
+  if [ "${#confirmed_packages[@]}" -eq "0" ]; then 
+cat << "EOT" > dangerfile.js
+const { markdown } = require('danger');
+
+const first_line = `:warning: NOTIFICATION :warning:`;
+const second_line = `You are receiving this message because there were no packages to publish..`;
+
+markdown(`${first_line}\n\n${second_line}`)
+EOT
+  else
+    install_with_CLI
+    for dir in ${confirmed_packages[@]}; do
+      cd $dir
+
+      authenticate_publish
+      npm_version_SHA
+
+      if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]; then
         npm publish --access=public --tag $tag
       else
         $INPUT_NPM_PUBLISH --access=public --tag $tag
-    fi
-
+      fi
+      
+      pkgname="`node -e \"console.log(require('./package.json').name)\"`"
+      echo $(jq --arg PKG "$pkgname" '.packages[.packages | length] |= . + {"name": $PKG}' $GITHUB_WORKSPACE/published.json) > $GITHUB_WORKSPACE/published.json
+      cd $GITHUB_WORKSPACE
+    done;
+    if [ "${#confirmed_packages[@]}" -eq "1" ]; then
 cat << "EOT" > dangerfile.js
 const { markdown } = require('danger');
 const pjson = require('./package.json');
@@ -73,21 +85,176 @@ const last_line = `Once the branch associated with this tag is deleted (usually 
 
 markdown(`${first_line}\n${second_line}\n\`\`\`bash\n${install_tag}\n\`\`\`\n${fourth_line}\n\`\`\`bash\n${update_json}\n\`\`\`\n${last_line}`)
 EOT
+    else
+cat << "EOT" > dangerfile.js
+const { markdown } = require('danger');
+const pjson = require('./published.json');
 
-  fi
+function packages(){
+  let stringy = "";
+  pjson.packages.map(x=>{
+    stringy += package(x.name)
+  })
+  return stringy;
+}
 
-jsonpath="../workflow/event.json"
-headurl="$(jq '."pull_request"|."head"|."repo"|."url"' $jsonpath)"
-baseurl="$(jq '."pull_request"|."base"|."repo"|."url"' $jsonpath)"
+function package(name){
+  const first_line = `Install using the following command:`
+  const install_tag = `$ npm install ${name}@${pjson.tag}`
+  const or_line = `Or update your package.json file:`
+  const json_line = `\"${name}\": \"${pjson.tag}\"`
 
-  if [[ "$headurl" = "$baseurl" ]]; 
-  then
-    yarn global add danger --dev
-    export PATH="$(yarn global bin):$PATH"
-    danger ci
+  return `<details><summary>${name}</summary>
+  
+  ---
+  ${first_line}
+  
+  \`\`\`bash
+  ${install_tag}
+  \`\`\`
+  ${or_line}
+  
+  \`\`\`bash
+  {
+    ${json_line}
+  }
+  \`\`\`
+  
+  ---
+  </details>`
+}
+
+const first_line = `The packages of this pull request have been released to Github Package Registry.`;
+const second_line = `Click on the following packages for instructions on how to install them:`;
+
+markdown(`${first_line}\n${second_line}\n${packages()}`)
+EOT
+    fi;
+  fi;
+  run_danger
+}
+
+function remove_packages_to_skip(){
+  function unslash_end_of_args(){
+    for to_unslash in $*; do
+      echo $to_unslash | sed 's:\/$::g';
+    done;
+  }
+
+  defaults=("node_modules" ".github")
+  skip_directories=($(unslash_end_of_args $INPUT_IGNORE) ${defaults[@]})
+  package_directories_array=(${package_directories[@]})
+
+  all_json=$(find . -name 'package.json');
+  total_packages_count=${#all_json[@]};
+
+  for skip_directory in ${skip_directories[@]}; do
+    for i in ${!package_directories_array[@]}; do
+      if [ "${package_directories_array[$i]}" = "." ]; then
+        if [ "$total_packages_count" = "1" ]; then
+          :
+        else
+          unset package_directories_array[$i]
+        fi
+      else
+        if [ $(echo "${package_directories_array[$i]}" | sed -E "s:^$skip_directory.*::") ]; then
+          :
+        else
+          unset package_directories_array[$i]
+        fi
+      fi
+    done
+  done
+
+  confirmed_packages=($(echo "${package_directories_array[@]}" | xargs -n1 | sort -u | xargs))
+
+  publish
+}
+
+function package_json_finder(){
+  function format_dit_giff(){
+    for to_format in $*; do 
+      if [ "$(echo $to_format | grep -c "/")" = "0" ]; then 
+        echo ".";
+      else 
+        echo $(echo $to_format | sed 's:\(.*\)\/.*:\1:g');
+      fi;
+    done;
+  }
+  
+  git checkout $GITHUB_BASE_REF
+  git checkout $GITHUB_HEAD_REF
+
+  diffs=$(git diff --name-only $GITHUB_BASE_REF..$GITHUB_HEAD_REF)
+  diff_directories=$(format_dit_giff $diffs)
+  diff_directories_array=(${diff_directories[@]})
+
+  package_directories=()
+
+  function json_locater(){
+    cd $GITHUB_WORKSPACE/$1
+    if [ ! -f "package.json" ]; then
+      if [ "$(echo $1 | grep -c "/")" = "1" ]; then
+        super_directory=$(echo $1 | sed 's:\(.*\)\/.*:\1:g');
+        json_locater $super_directory
+      else
+        package_directories+=(".")
+      fi
+    else
+      package_directories+=("$1")
+    fi
+    cd $GITHUB_WORKSPACE
+  }
+
+  for i in ${!diff_directories_array[@]}; do 
+    json_locater ${diff_directories_array[$i]}
+  done;
+
+  remove_packages_to_skip
+}
+
+function check_prerequisites(){
+  PR="$(jq '."pull_request"' ../workflow/event.json)"
+  jsonpath="../workflow/event.json"
+  headurl="$(jq '."pull_request"|."head"|."repo"|."url"' $jsonpath)"
+  baseurl="$(jq '."pull_request"|."base"|."repo"|."url"' $jsonpath)"
+  
+  if [[ "$PR" = "null" ]]; then
+    echo -e "${RED}Not generating comment because this is not a pull request.${NC}"
   else
-    echo -e "${RED}Not generating a comment because this pull request was created from a forked repository.${NC}"
-    echo -e "${YELLOW}Publishing preview will not work if the pull request was created from a forked repository unless you create the pull request against your own repository.${NC}"
-  fi
+    if [[ "$headurl" != "$baseurl" ]]; then
+      echo -e "${RED}Not generating a comment because this pull request was created from a forked repository.${NC}"
+      echo -e "${YELLOW}Publishing preview will not work if the pull request was created from a forked repository unless you create the pull request against your own repository.${NC}"
+    else
+      if [[ "$GITHUB_HEAD_REF" = "latest" ]]; then
+        echo -e "${RED}ERROR: Unable to publish preview because your branch conflicts with NPM's protected 'latest' tag.${NC}"
+        echo -e "${YELLOW}Please change the name of your branch and resubmit the pull request.${NC}"
+cat << "EOT" > dangerfile.js
+const { markdown } = require('danger');
 
-fi
+const first_line = `:warning: WARNING :warning:`;
+const second_line = `I can't publish a preview of this branch this because it would conflict with the \`latest\` tag which is [reserved by NPM](https://docs.npmjs.com/cli/dist-tag#purpose).`;
+
+markdown(`${first_line}\n\n${second_line}`)
+EOT
+        run_danger
+      elif [ "${#INPUT_GPR}" -eq "0" ] && [ "${#NPM_AUTH_TOKEN}" -eq "0" ]; then
+        echo -e "${RED}ERROR: NPM_AUTH_TOKEN not detected. Please add your NPM Token to your repository's secrets.${NC}"
+    
+cat << "EOT" > dangerfile.js
+const { markdown } = require('danger');
+
+const first_line = `:mega: Heads up!`;
+const second_line = `I didn't detect an NPM_AUTH_TOKEN which is necessary to publish a preview version of this package, and so I wasn't able to. However, this is perfectly normal for pull requests that are submitted from a forked repository, so no need to worry if that's what's going on here.`;
+
+markdown(`${first_line}\n\n${second_line}`)
+EOT
+        run_danger
+      else
+        package_json_finder
+      fi
+    fi
+  fi
+}
+
+check_prerequisites
