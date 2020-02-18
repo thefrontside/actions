c872221d59db6502933d3ae0a9b3b4eee0722183
minkimcello@gmail.com <minkimcello@gmail.com>
2020-02-18 15:43:36 -0500

    Add monorepo support for release action

--- synchronize-with-npm/entrypoint.sh
+++ synchronize-with-npm/entrypoint.sh
@@ -4,39 +4,148 @@ set -e
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
 NC='\033[0m'
 
-if [ "${#NPM_TOKEN}" -eq "0" ]
-  then
-    echo -e "${RED}NPM_TOKEN not detected. Please add your NPM Token to your repository's secrets.${NC}"
-  else 
-    version="`node -e \"console.log(require('./package.json').version)\"`"
-    package="`node -e \"console.log(require('./package.json').name)\"`"
-    if [ -z "$(npm view $package@$version)" ]
-      then
-        git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git
-        git fetch origin +refs/heads/*:refs/heads/*
+function git_setup(){
+  git remote set-url origin https://x-oauth-basic:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+  git fetch origin +refs/heads/*:refs/heads/*
 
-        branch="${GITHUB_REF#*refs\/heads\/}"
-        git checkout $branch
+  branch="${GITHUB_REF#*refs\/heads\/}"
+  git checkout $branch
 
-        git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-        git config user.name "$GITHUB_ACTOR"
-        
-        git tag --force "v$version"
-        git push --force  --tags "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+  git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+  git config user.name "$GITHUB_ACTOR"
+}
 
-        echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-        npm config set unsafe-perm true
-        npm install
-        if [ "${#INPUT_NPM_PUBLISH}" -eq "0" ]
-          then
-            npm publish --access=public
+function get_directories(){
+  function format_dit_giff(){
+    for to_format in $*; do 
+      if [ "$(echo $to_format | grep -c "/")" = "0" ]; then 
+        echo ".";
+      else 
+        echo $(echo $to_format | sed 's:\(.*\)\/.*:\1:g');
+      fi;
+    done;
+  }
+
+  function unslash_end(){
+    for to_unslash in $*; do
+      echo $to_unslash | sed 's:\/$::g';
+    done;
+  }
+
+  function json_locater(){
+    echo -e "${GREEN}Running json_locator for: ${YELLOW}$1${NC}"
+    if [ -d ${GITHUB_WORKSPACE}/$1 ]; then
+      cd $GITHUB_WORKSPACE/$1        
+      if [ ! -f "package.json" ]; then
+        if [ "$(echo $1 | grep -c "/")" = "1" ]; then
+          super_directory+=$(echo $1 | sed 's:\(.*\)\/.*:\1:g');
+          json_locater $super_directory
+        else
+          cd $GITHUB_WORKSPACE
+          json_within=($(find . -name 'package.json' -not -path './node_modules/*'));
+          json_count=${#json_within[@]};
+          if [ "$json_count" != "1" ]; then
+            echo -e "${RED}Excluding: ${YELLOW}.${RED} because there is a sub-package.${NC}"
           else
-            $INPUT_NPM_PUBLISH --access=public
+            package_directories+=(".")
+          fi
         fi
-        echo -e "${GREEN}Tagged and published version v${version} successfully!${NC}"
       else
-        echo -e "${YELLOW}Version $version of this package already exists. To publish the changes of this commit, you must update package version in the JSON file of your project.${NC}"
+        json_within=($(find . -name 'package.json' -not -path './node_modules/*'));
+        json_count=${#json_within[@]};
+        if [ "$json_count" != "1" ]; then
+          echo -e "${RED}Excluding: ${YELLOW}$1${RED} because there is a sub-package.${NC}"
+        else
+          package_directories+=("$1")
+        fi
+      fi
+      cd $GITHUB_WORKSPACE
+    else
+      echo -e "${RED}Skipping ${YELLOW}$1${RED} because the directory does not exist.${NC}"
     fi
-fi
+  }
+
+  function filter_ignores(){
+    defaults=("node_modules" ".github")
+    skip_directories=($(unslash_end $INPUT_IGNORE) ${defaults[@]})
+    for skip_directory in ${skip_directories[@]}; do
+      for i in ${!package_directories[@]}; do
+        if [ $(echo "${package_directories[$i]}" | sed -E "s:^$skip_directory.*::") ]; then
+          :
+        else
+          echo -e "${RED}Removing ${YELLOW}${package_directories[$i]} ${RED}because of ${YELLOW}${skip_directory}${NC}"
+          unset package_directories[$i]
+        fi
+      done
+    done
+  }
+
+  before=$(git --no-pager log --pretty=%P -n 1 $GITHUB_SHA)
+  current=$GITHUB_SHA
+  diff_directories=($(echo $(format_dit_giff $(git diff --name-only $before..$current)) | xargs -n1 | sort -u | xargs))
+  package_directories=()
+
+  for i in ${!diff_directories[@]}; do
+    json_locater ${diff_directories[$i]}
+  done
+
+  filter_ignores
+
+  publish_directories=($(echo ${package_directories[@]} | xargs -n1 | sort -u | xargs))
+}
+
+function publish(){
+  function install_with_CLI(){
+    if [ -f "yarn.lock" ]; then
+      echo -e "${YELLOW}Installing with yarn...${NC}"
+      npm_config_unsafe_perm=true yarn
+    else
+      echo -e "${YELLOW}Installing with npm...${NC}"
+      npm_config_unsafe_perm=true npm install
+    fi
+  }
+
+  function publish_command(){
+    if [ "${#INPUT_NPM_PUBLISH}" = "0" ]; then
+      npm publish
+    else
+      $INPUT_NPM_PUBLISH
+    fi
+  }
+
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+  echo "unsafe-perm=true" >> ~/.npmrc
+
+  install_with_CLI
+
+  for package in ${publish_directories[@]}; do
+    cd $GITHUB_WORKSPACE/$package
+
+    is_private=$(echo $(jq '.private' package.json))
+
+    if [ "$is_private" = "true" ]; then
+      echo -e "${RED}Skipping publishing process for: ${YELLOW}$dir${RED} because package is marked as private and therefore not intended to be published.${NC}"
+    else
+      package_name="`node -e \"console.log(require('./package.json').name)\"`"
+      version="`node -e \"console.log(require('./package.json').version)\"`"
+      echo -e "${GREEN}Running publishing process for: ${YELLOW}$package_name${NC}"
+
+      if [ -z "$(npm view ${package_name}@${version})" ]; then
+        publish_command --access=public
+        echo -e "${GREEN}Successfully published version ${BLUE}${version}${GREEN} of ${BLUE}${package_name}${GREEN}!${NC}"
+      else
+        echo -e "${RED}Version ${YELLOW}$version${RED} of ${YELLOW}$package_name${RED} already exists.${NC}"
+        echo -e "${BLUE}Tip:${YELLOW}To publish the changes of this commit, you must update package version in the package.json file.${NC}"
+      fi
+    fi
+
+    cd $GITHUB_WORKSPACE
+  done
+}
+
+git_setup
+get_directories
+publish
