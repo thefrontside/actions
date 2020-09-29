3b0fe1bf539c787a1050b986aab467f62ed5eb4c
minkimcello <minkimcello@gmail.com>
2020-09-29 17:51:32 -0400

    Update tested yarn action and READMEs

--- yarn/entrypoint.sh
+++ yarn/entrypoint.sh
@@ -2,15 +2,15 @@
 set -e
 
 RED='\033[1;31m'
-GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-# WARN
-# warning
-# error
+yarn > stderr.log 2>&1
+yarnoutput="$(cat stderr.log)"
+echo "$yarnoutput"
+warnCount="$(echo $yarnoutput | grep -c -i "warn")"
 
-# check if spell casing has an effect when you filter
-# check if action does yarn, does it persist?
-
-yarn
+if [ $warnCount != "0" ]; then
+  echo -e "${RED}ERROR:${YELLOW} Halting workflow because a warning was detected while installing...${NC}"
+  exit 1
+fi;
