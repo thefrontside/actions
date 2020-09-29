a43d86d9dab3b42ebba0ae79de28629aef11581d
minkimcello <minkimcello@gmail.com>
2020-09-29 11:49:55 -0400

    Yarn action

---
+++ yarn/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+# WARN
+# warning
+# error
+
+# check if spell casing has an effect when you filter
+# check if action does yarn, does it persist?
+
+yarn
