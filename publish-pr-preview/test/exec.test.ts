45d1cfc154a4ad0fdaa28f7488cb14e63dd3e337
minkimcello <minkimcello@gmail.com>
2021-10-27 21:09:59 -0400

    Test for double checking effection exec cwd option

---
+++ publish-pr-preview/test/exec.test.ts
@@ -0,0 +1,46 @@
+import { describe, it, beforeEach } from "@effection/mocha";
+import { exec } from "@effection/process";
+import fs, { promises as fsp } from "fs";
+import expect from "expect";
+
+describe("exec with cwd for npm commands", () => {
+  beforeEach(function* () {
+    if (fs.existsSync(`${__dirname}/fixtures`)) {
+      yield exec("rm -rf fixtures", { cwd: __dirname }).expect();
+    }
+    let fixturePackageJson = (name: string) => {
+      return JSON.stringify({ name: name, version: "0.0.0", private: true }, null, 2);
+    };
+    yield function* () {
+      yield fsp.mkdir(`${__dirname}/fixtures`);
+      yield fsp.mkdir(`${__dirname}/fixtures/pkg-a`);
+      yield fsp.mkdir(`${__dirname}/fixtures/pkg-b`);
+      yield fsp.appendFile(
+        `${__dirname}/fixtures/pkg-a/package.json`,
+        fixturePackageJson("pkg-a")
+      );
+      yield fsp.appendFile(
+        `${__dirname}/fixtures/pkg-b/package.json`,
+        fixturePackageJson("pkg-b")
+      );
+    };
+  });
+  it("exec npm view in fixtures", function* () {
+    yield exec(
+      "npm version 1.0.0 --no-git-tag-version",
+      { cwd: `${__dirname}/fixtures/pkg-a` }
+    ).expect();
+    yield exec(
+      "npm version 2.0.0 --no-git-tag-version",
+      { cwd: `${__dirname}/fixtures/pkg-b` }
+    ).expect();
+    let pkgA = JSON.parse(
+      fs.readFileSync(`${__dirname}/fixtures/pkg-a/package.json`, { encoding: "utf-8" })
+    );
+    let pkgB = JSON.parse(
+      fs.readFileSync(`${__dirname}/fixtures/pkg-b/package.json`, { encoding: "utf-8" })
+    );
+    expect(pkgA.version).toBe("1.0.0");
+    expect(pkgB.version).toBe("2.0.0");
+  });
+});
