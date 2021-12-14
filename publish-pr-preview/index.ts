c6f79f6e391091347b88750a63398e2ab3bfa337
minkimcello <minkimcello@gmail.com>
2021-12-14 08:16:49 -0500

    Tidy up preview action

--- publish-pr-preview/index.ts
+++ publish-pr-preview/index.ts
@@ -4,19 +4,11 @@ import fs from "fs";
 import { main } from "effection";
 import { run } from "./src";
 
-// TODO we don't have token input so just do `process... || ""`
-const token =
-  core.getInput("token") === ""
-    ? process.env.GITHUB_TOKEN || ""
-    : core.getInput("token");
+const token = process.env.GITHUB_TOKEN || ""
 const octokit = github.getOctokit(token);
 
 const payload = JSON.parse(fs.readFileSync(`${process.env.GITHUB_EVENT_PATH}`, "utf-8"));
 
 main(
-  run({
-    octokit,
-    core,
-    payload,
-  })
+  run({ octokit, core, payload })
 );
